9b6e7c5709ecef3f1c366168733846baee201fe7
Brittan Fairchild <brittanfairchild@gmail.com>
2019-07-12 20:02:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# Your Code Here
+def map(array)
+  new_array = []
+
+  for element in array
+    new_array.push yield element
+  end
+
+  new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  
+  
+end
+
